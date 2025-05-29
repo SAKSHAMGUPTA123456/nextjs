a1250fe18b9580dc136488c435fdb451cae3bf73
SAKSHAMGUPTA123456 <sakshamgupta0155.in>
2025-05-29 18:41:55 +0530

    s

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // disables server-side image optimization
+  },
+};
+
+module.exports = nextConfig;
